ffa4221a9ab2077e5869d079134e688c6bf62425
Faiz <43031284+cilikan@users.noreply.github.com>
2021-11-14 22:35:12 +0700

    Add files via upload

---
+++ Tugas_App/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_App
+{
+    //inheritance
+    class Invoice : Payment
+    {
+        public string[] Type { get; set; }
+        public int Choice { get; set; }
+
+        public void InvoicePembelian()
+        {
+            Console.WriteLine("--- NOTA PEMBELIAN TOKO SEPEDA MAJU MAKMUR---");
+            Console.WriteLine("Barang yang Dibeli : " + Type[Choice]);
+            Console.WriteLine("Jumlah Barang : " + Quantity);
+            TotalPrice();
+            Console.WriteLine("--- JIKA PUAS, BERITAHU TEMAN ---");
+            Console.WriteLine("--- JIKA TAK PUAS, BERITAHU KAMI ---");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
